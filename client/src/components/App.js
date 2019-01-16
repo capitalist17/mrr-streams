54d59d2523b936ec5b066bf887c14357913ff0da
Arvind SN <asn@asn.com>
2019-01-16 16:14:33 +0530

    Use of Link tag instead of an anchor tag

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 const PageOne = () => {
-  return <div>Page one</div>
+  return <div>Page one <Link to="/pageTwo"> go to pg2 </Link> </div>
 }
 const PageTwo = () => {
-  return <div>Page two <button>Click Me!</button> </div>
+  return <div>Page two <button>Click Me!</button> 
+    <Link to="/"> go to pg1 </Link>
+  </div>
 }
 
 class App extends Component {
