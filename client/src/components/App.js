6d94c9e9d07df4e9d06f979fd677619699efa181
Arvind SN <asn@asn.com>
2019-01-16 16:57:55 +0530

    Component scaffolding and wiring up the routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,23 +1,23 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 
-const PageOne = () => {
-  return <div>Page one <Link to="/pageTwo"> go to pg2 </Link> </div>
-}
-const PageTwo = () => {
-  return <div>Page two <button>Click Me!</button> 
-    <Link to="/"> go to pg1 </Link>
-  </div>
-}
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
 
 class App extends Component {
   render() {
     return (<BrowserRouter>
-    <div>
-      <Route path="/" exact component={PageOne} />
-      <Route path="/pagetwo" component={PageTwo} />
-    </div>
-  </BrowserRouter>)
+      <div>
+        <Route path="/" exact component={StreamList} />
+        <Route path="/streams/new" exact component={StreamCreate} />
+        <Route path="/streams/edit" exact component={StreamEdit} />
+        <Route path="/streams/delete" exact component={StreamDelete} />
+        <Route path="/streams/show" exact component={StreamShow} />
+      </div>
+    </BrowserRouter>)
   }
 }
 
