e6ead1a9e6e150a2d2d009a25e9f883f63454514
Arvind SN <asn@asn.com>
2019-01-17 14:11:24 +0530

    Creating forms, connectin it to redux, and  handling events automatically

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
 
 class StreamCreate extends Component {
+    renderInput({input}){
+        return <input {...input} />
+    }
     render() {
-        return (
-            <div>
-                StreamCreate
-            </div>
+        //console.log(this.props)
+        return (            
+            <form>
+                <Field name="title" component={this.renderInput}/>
+                <Field name="description" component={this.renderInput}/>
+            </form>
         );
     }
 }
 
-export default StreamCreate;
+const formWrapped = reduxForm({
+                                form: 'streamCreate'
+                            })(StreamCreate);
+export default connect()(formWrapped);
+
+// This causes error in the console. Due to redux-form ver-8 (latest version)
+// export default reduxForm({ form: 'streamCreate' }) (StreamCreate);
