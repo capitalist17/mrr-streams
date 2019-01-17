f4256a019573c9262b5736cffd76dd22b9d865f1
Arvind SN <asn@asn.com>
2019-01-17 17:17:18 +0530

    Creating a stream using rest conventions

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { connect } from 'react-redux';
+import { createStream } from '../../actions';
 
 class StreamCreate extends Component {
     renderError({error, touched}){
@@ -23,8 +24,9 @@ class StreamCreate extends Component {
             </div>
             )
     }
-    onSubmit(formValues){
-        console.log("Form Submit- Redux-form magic", formValues)
+    onSubmit = (formValues) => {
+        //console.log("Form Submit- Redux-form magic", formValues)
+        this.props.createStream(formValues)
     }
 
     render() {
@@ -57,7 +59,8 @@ const formWrapped = reduxForm({
                                 form: 'streamCreate',
                                 validate: validate
                             })(StreamCreate);
-export default connect()(formWrapped);
+
+export default connect(null,{createStream})(formWrapped);
 
 // This causes error in the console. Due to redux-form ver-8 (latest version)
 // export default reduxForm({ form: 'streamCreate' }) (StreamCreate);
