7824fc70b0e796d4e448b5e7b09a4d0dc51f3a94
Arvind SN <asn@asn.com>
2019-01-17 14:30:10 +0530

    Handling form submissions

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -11,12 +11,17 @@ class StreamCreate extends Component {
             </div>
             )
     }
+    onSubmit(formValues){
+        console.log("Form Submit- Redux-form magic", formValues)
+    }
+
     render() {
         //console.log(this.props)
         return (            
-            <form className="ui form">
+            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form">
                 <Field name="title" label="Enter Title" component={this.renderInput}/>
                 <Field name="description" label="Enter Description" component={this.renderInput}/>
+                <button className="ui button primary">Submit</button>
             </form>
         );
     }
