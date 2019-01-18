ded70d760d20e4e096e8256e11b6ec7dc2ac589e
Arvind SN <asn@asn.com>
2019-01-18 13:29:22 +0530

    Code reuse - StreamForm can be used to both create and edit streams

---
+++ client/src/components/streams/StreamForm.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+
+class StreamForm extends Component {
+    renderError({error, touched}){
+        if (touched && error) {
+            return (
+              <div className="ui error message">
+                <div className="header">{error}</div>
+              </div>
+            );
+          }
+    }
+    renderInput = ({input, label, meta}) => {
+        //console.log(meta)
+        const classStyle = `field ${meta.error && meta.touched ? 'error' : ''}`;
+        return (
+            <div className={classStyle} >
+                <label>{label}</label>
+                <input {...input} autoComplete="off" />
+                {this.renderError(meta)}
+            </div>
+            )
+    }
+    onSubmit = (formValues) => {
+        //console.log("Form Submit- Redux-form magic", formValues)
+        this.props.onSubmit(formValues)
+    }
+
+    render() {
+        //console.log(this.props)
+        return (            
+            <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
+                <Field name="title" label="Enter Title" component={this.renderInput}/>
+                <Field name="description" label="Enter Description" component={this.renderInput}/>
+                <button className="ui button primary">Submit</button>
+            </form>
+        );
+    }
+}
+
+const validate = formValues => {
+    const errors = {};
+
+    if (!formValues.title) {
+        errors.title = 'You must enter a title';
+    }
+
+    if (!formValues.description) {
+        errors.description = 'You must enter a description';
+    }
+
+    return errors;
+};
+
+const formWrapped = reduxForm({
+                                form: 'StreamForm',
+                                validate: validate
+                            })(StreamForm);
+
+export default (formWrapped);
+
+// This causes error in the console. Due to redux-form ver-8 (latest version)
+// export default reduxForm({ form: 'streamCreate' }) (StreamCreate);
