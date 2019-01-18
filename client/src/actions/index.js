66d93f2cabdbe0894efaa778927e1b499b00d06c
Arvind SN <asn@asn.com>
2019-01-18 14:14:33 +0530

    Edit form submission

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -44,8 +44,9 @@ export const fetchStream = (id) => {
 
 export const editStream = (id, formValues) => { 
     return async (dispatch) => {
-        const response = await streams.patch(`/streams/${id}`, formValues);
+        const response = await streams.put(`/streams/${id}`, formValues);
         dispatch({ type: EDIT_STREAM, payload: response.data });
+        history.push('/');
     }
 };
 
--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,10 +1,17 @@
+import _ from 'lodash'
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchStream } from '../../actions';
+import { fetchStream, editStream } from '../../actions';
+import StreamForm from './StreamForm';
 
 class StreamEdit extends Component {
     componentDidMount() {
         this.props.fetchStream(this.props.match.params.id);
+    }
+
+    onSubmit = (formValues) => {
+        // console.log(formValues)
+        this.props.editStream(this.props.match.params.id, formValues)
     }
 
     render() {
@@ -13,7 +20,13 @@ class StreamEdit extends Component {
             return <div>Loading...</div>;
         }
     
-        return <div>{this.props.stream.title}</div>;
+        return (
+            <div>
+                <h3>Edit a Stream</h3>
+                <StreamForm initialValues={_.pick(this.props.stream, 'title', 'description')} 
+                            onSubmit={this.onSubmit} />
+            </div>
+        );
     }
 }
 
@@ -21,4 +34,4 @@ const mapStateToProps = (state, ownProps) => {
     return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default connect(mapStateToProps,{fetchStream})(StreamEdit);
+export default connect(mapStateToProps,{fetchStream, editStream})(StreamEdit);
