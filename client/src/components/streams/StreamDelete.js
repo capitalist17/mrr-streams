820b8a2b2f05e377513e792250c6fa007437a1e4
Arvind SN <asn@asn.com>
2019-01-18 15:06:43 +0530

    Using Portals to create modal functionality in ReactJS - Important concept

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Modal from '../Modal';
 
 class StreamDelete extends Component {
     render() {
         return (
             <div>
                 StreamDelete
+                <Modal />
             </div>
         );
     }
