458727a1153689dda3610827fa3b88c244bf7ecf
Arvind SN <asn@asn.com>
2019-01-18 15:40:48 +0530

    Reusable Modal and React Fragment illustration

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import Modal from '../Modal';
 
-class StreamDelete extends Component {
+class StreamDelete extends Component {    
     render() {
+        const actions = (
+            <React.Fragment>
+                <button className="ui button negative">Delete</button>
+                <button className="ui button">Cancel</button>
+            </React.Fragment>
+        );
+    
         return (
             <div>
                 StreamDelete
-                <Modal />
+                <Modal 
+                    title="Delete Stream"
+                    content="Are you sure you want to delete this stream?"
+                    actions = {actions}
+                />
             </div>
         );
     }
